7acfd22f8938b1b4eb62ba295a5266d0ab4af103
tcolbath <trevor.colbath@gmail.com>
2024-03-17 12:24:23 -0400

    Graphics complete, cell started

---
+++ src/cell.py
@@ -0,0 +1,76 @@
+from graphics import Window, Line, Point
+
+
+class Cell:
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = window
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        
+        if self.has_top_wall == True:
+            tw = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(tw)
+        if self.has_right_wall == True:
+            rw = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(rw)
+        if self.has_bottom_wall == True:
+            bw = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(bw)
+        if self.has_left_wall == True:
+            lw = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(lw)
+
+    def draw_move(self, to_cell, undo=False):
+        if self._win is None:
+            return
+        
+        # center coords
+        x_mid = (self._x1 + self._x2) / 2
+        y_mid = (self._y1 + self._y2) / 2
+
+        to_x_mid = (to_cell._x1 + to_cell._x2) / 2
+        to_y_mid = (to_cell._y1 + to_cell._y2) / 2
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        # moving left
+        if self._x1 > to_cell._x1:
+            line = Line(Point(self._x1, y_mid), Point(x_mid, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell._x2, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # moving right
+        elif self._x1 < to_cell._x1:
+            line = Line(Point(x_mid, y_mid), Point(self._x2, y_mid))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_cell._x1, to_y_mid), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # moving up
+        elif self._y1 > to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y1))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell._y2), Point(to_x_mid, to_y_mid))
+            self._win.draw_line(line, fill_color)
+
+        # moving down
+        elif self._y1 < to_cell._y1:
+            line = Line(Point(x_mid, y_mid), Point(x_mid, self._y2))
+            self._win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_x_mid, to_cell._y1))
+            self._win.draw_line(line, fill_color)
--- src/graphics.py
+++ src/graphics.py
@@ -20,5 +20,26 @@ class Window:
             self.redraw()
         print("Window closed....")
 
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
+
     def close(self):
         self.__running = False
+    
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.p1 = point1
+        self.p2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+
+    
